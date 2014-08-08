e026946d43f904e146f1abcf7a60abdc9404dc4a
Marcel du Preez <marceldupr@gmail.com>
2014-08-08 11:22:10 +0100

    Added release folder

---
+++ Server/src/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+using Server;
+
+[assembly: OwinStartup(typeof (Startup), "Configuration")]
+
+namespace Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalHost.DependencyResolver.UseSqlServer(
+                ConfigurationManager.ConnectionStrings["SignalR"].ConnectionString);
+
+            app.MapSignalR();
+        }
+    }
+}
